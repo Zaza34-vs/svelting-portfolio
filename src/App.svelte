27ac6cc6ed33b961928f849f922c32b831150099
hamza <zazarebba975@gmail.com>
2022-06-22 11:59:52 +0200

    start my portfolio in svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
+	import Header from './header.svelte';
+	import Section from './section.svelte';
+	import Footer from './footer.svelte';
+
 	export let name: string;
 	name = "Friend";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
+	<Header/>
+	<Section/>
+	<Footer/>
+
 </main>
 
 <style type="scss">
---
+++ src/header.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<header>
+    <h1>Portfolio Perso</h1>
+</header>
