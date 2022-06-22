79a78397b6a64c5c400e39854941f4218573beb6
hamza <zazarebba975@gmail.com>
2022-06-22 17:32:05 +0200

    end of the header dev

--- src/App.svelte
+++ src/App.svelte
@@ -2,25 +2,10 @@
 	import Header from './header.svelte';
 	import Section from './section.svelte';
 	import Footer from './footer.svelte';
-
-	export let name: string;
-	name = "Friend";
 </script>
 
-<body>
-	<header>
-		<Header/>
-	</header>
-	
-	<section>
-		<Section/>
-	</section>
-	
-	<footer>
-		<Footer/>
-	</footer>
-</body>
+<Header/>
 
-<style type="scss">
-	@use '../sass/main.scss';
-</style>
+<Section/>
+
+<Footer/>
--- src/header.svelte
+++ src/header.svelte
@@ -1,6 +1,4 @@
-<script lang="ts">
-
-</script>
+<script lang="ts"></script>
 
 <header>
     <h1>Portfolio Perso</h1>
