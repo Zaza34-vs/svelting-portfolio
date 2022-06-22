f00dd85c779530941b01750f7df55efbc5222fa8
hamza <zazarebba975@gmail.com>
2022-06-22 09:52:49 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let name: string;
+	name = "Friend";
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style type="scss">
+	@use '../sass/main.scss';
+</style>
