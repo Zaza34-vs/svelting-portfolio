bffdda47c1278e7a1a5b25e7ea26a474769a4463
hamza <zazarebba975@gmail.com>
2022-06-28 17:25:45 +0200

    continuation of the footer

--- src/footer.svelte
+++ src/footer.svelte
@@ -3,21 +3,34 @@
     <h2 class="h2_foot">
         Pour me suivre :
     </h2>
-    <a href="https://github.com/Zaza34-vs">
-        <img class="icons" rel='icon' type='image/png' src="github.png" alt="">
-    </a>
-    <a href="https://gitlab.com/hamza_r">
-        <img class="icons" rel='icon' type='image/png' src="gitlab.png" alt="">
-    </a>
-    <a href="https://www.linkedin.com/in/hamza-rebbani/">
-        <img class="icons" rel='icon' type='image/png' src="linkedin.png" alt="">
-    </a>
-    <a href="https://codepen.io/zaza34-vs">
-        <img class="icons" rel='icon' type='image/png' src="codepen.png" alt="">
-    </a>
-    <a href="https://www.codingame.com/profile/fed03a3133e2ccc334bc094c24fe15b64243624">
-        <img class="icons" rel='icon' type='image/png' src="codein.png" alt="">
-    </a>
+    <div class="url_social">
+        <a href="https://github.com/Zaza34-vs">
+            <p class="h2_foot">
+                Sur GitHub
+            </p>
+        </a>
+        <a href="https://gitlab.com/hamza_r">
+            <p class="h2_foot">
+                Sur GitLab
+            </p>
+        </a>
+        <a href="https://www.linkedin.com/in/hamza-rebbani/">
+            <p class="h2_foot">
+                Sur Linkedin
+            </p>
+        </a>
+        <a href="https://codepen.io/zaza34-vs">
+            <p class="h2_foot">
+                Sur CodePen
+            </p>
+            <div class="codepen"></div>
+        </a>
+        <a href="https://www.codingame.com/profile/fed03a3133e2ccc334bc094c24fe15b64243624">
+            <p class="h2_foot">
+                Sur CodinGame
+            </p>
+        </a>
+    </div>
 </footer>
 
 <style type="scss">
