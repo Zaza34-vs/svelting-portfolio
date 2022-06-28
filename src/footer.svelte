a1ab4005e9966228da7ceac1ae380a4e82eb58a6
hamza <zazarebba975@gmail.com>
2022-06-28 16:55:47 +0200

    footer management

--- src/footer.svelte
+++ src/footer.svelte
@@ -4,19 +4,19 @@
         Pour me suivre :
     </h2>
     <a href="https://github.com/Zaza34-vs">
-        <img class="icons" src="../icons/github.png" alt="">
+        <img class="icons" rel='icon' type='image/png' src="github.png" alt="">
     </a>
     <a href="https://gitlab.com/hamza_r">
-        <img class="icons" src="../icons/gitlab.png" alt="">
+        <img class="icons" rel='icon' type='image/png' src="gitlab.png" alt="">
     </a>
     <a href="https://www.linkedin.com/in/hamza-rebbani/">
-        <img class="icons" src="../icons/linkedin.png" alt="">
+        <img class="icons" rel='icon' type='image/png' src="linkedin.png" alt="">
     </a>
     <a href="https://codepen.io/zaza34-vs">
-        <img class="icons" src="../icons/codepen.png" alt="">
+        <img class="icons" rel='icon' type='image/png' src="codepen.png" alt="">
     </a>
     <a href="https://www.codingame.com/profile/fed03a3133e2ccc334bc094c24fe15b64243624">
-        <img class="icons" src="../icons/codein.png" alt="">
+        <img class="icons" rel='icon' type='image/png' src="codein.png" alt="">
     </a>
 </footer>
 
